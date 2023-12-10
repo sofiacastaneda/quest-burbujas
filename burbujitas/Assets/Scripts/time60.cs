6b91ceee5d507c229e6f37b6594504c9a8ebbac9
s/c <47247552+sofiacastaneda@users.noreply.github.com>
2023-12-09 22:37:09 -0500

    bbb

--- burbujitas/Assets/Scripts/time60.cs
+++ burbujitas/Assets/Scripts/time60.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class time60 : MonoBehaviour
 {
+    [SerializeField] int min, seg;
+    [SerializeField] TextMeshPro tiempo;
+
+    private float restante;
+    private bool enMarcha;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        restante = seg;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (enMarcha)
+        {
+            restante -= Time.deltaTime;
+            if (restante <1)
+            {
+                //Nubecitas
+            }
+            //int tempMin = Mathf.FloorToInt(restante);
+            int tempSeg = Mathf.FloorToInt(restante);
+            tiempo.text = string.Format("{00:00}",restante);
+        }
     }
 }
